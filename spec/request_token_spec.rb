ab46415bdabf71db3d10cdca42d7eb8e641f39be
Edwin Vlieg <e.vlieg@bluetools.nl>
2011-03-16 16:08:50 +0100

    Tests

--- spec/request_token_spec.rb
+++ spec/request_token_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Request token" do
   
-  context "Success" do
+  context "Success", :wip => true do
     
     before(:all) do
       @client_application = Factory.create(:client_application)
@@ -43,6 +43,45 @@ describe "Request token" do
       WardenOauthProvider::Token::Request.where(:token => @oauth_response["oauth_token"], :secret => @oauth_response["oauth_token_secret"]).count.should == 1
     end
     
+  end
+  
+  context "Success with GET", :wip => true do
+    before(:all) do
+      @client_application = Factory.create(:client_application)
+      
+      auth_params = {
+        :realm                  => "MoneyBird",
+        :oauth_consumer_key     => @client_application.key,
+        :oauth_signature_method => "PLAINTEXT",
+        :oauth_timestamp        => Time.now.to_i,
+        :oauth_nonce            => Time.now.to_f,
+        :oauth_callback         => "oob",
+        :oauth_signature        => @client_application.secret+"&"
+      }
+      
+      env = env_with_params("/oauth/request_token", auth_params, {})
+      @response = setup_rack.call(env)
+      @oauth_response = Hash[*@response.last.first.split("&").collect { |v| v.split("=") }.flatten]
+    end
+    
+    it "should have an oauth token" do
+      @oauth_response.keys.should include("oauth_token")
+      @oauth_response["oauth_token"].should_not be_nil
+    end
+    
+    it "should have an oauth token secret" do
+      @oauth_response.keys.should include("oauth_token_secret")
+      @oauth_response["oauth_token_secret"].should_not be_nil
+    end
+    
+    it "should have an oauth callback confirmed header" do
+      @oauth_response.keys.should include("oauth_callback_confirmed")
+      @oauth_response["oauth_callback_confirmed"].should == "true"
+    end
+    
+    it "should have created a new request token in the database" do
+      WardenOauthProvider::Token::Request.where(:token => @oauth_response["oauth_token"], :secret => @oauth_response["oauth_token_secret"]).count.should == 1
+    end
   end
   
   context "Failure" do
